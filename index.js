9ef388e092405e583b08c00ddabf8a0ded47c6c5
IanYet <yt2015.97@gmail.com>
2020-07-30 20:26:44 +0800

    完善文件结构

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 const { program } = require('commander')
 const { version, description } = require('./package.json')
 const fs = require('fs')
+const { dir } = require('console')
 const fsp = fs.promises
 
 const l = console.log
+const splitSign = '/'
 
 const createProject = (path) => {
     l('\n🏁: Start!\n')
+
+    const tarPath = process.cwd() + splitSign + path
+    const srcPath = __dirname + splitSign + 'source'
 
     const version = Number(process.version.substr(1).split('.')[0])
     if (version < 12) {
@@ -17,15 +22,69 @@ const createProject = (path) => {
         return
     }
 
-    fsp.mkdir(path)
-        .then(() => fsp.readdir('source'))
+    fsp.mkdir(tarPath)
+        .then(() => fsp.readdir(srcPath, { withFileTypes: true }))
         .then((dirs) => {
+            const all = []
+
             dirs.forEach((dir) => {
-                console.log(dir, typeof dir)
+                if (dir.isFile()) {
+                    all.push(
+                        fsp.copyFile(
+                            srcPath + splitSign + dir.name,
+                            tarPath + splitSign + dir.name
+                        )
+                    )
+                } else {
+                    all.unshift(dir)
+                }
             })
+
+            return Promise.all(all)
+        })
+        .then((dirs) =>
+            Promise.all([
+                ...dirs
+                    .filter((dir) => dir && dir.isDirectory())
+                    .map((dir) =>
+                        fsp.readdir(srcPath + splitSign + dir.name, {
+                            withFileTypes: true,
+                        })
+                    ),
+                dirs,
+            ])
+        )
+        .then((dirs) => {
+            const ds = dirs[dirs.length - 1]
+
+            ds.filter((d) => d).forEach((d) =>
+                fsp.mkdir(tarPath + splitSign + d.name)
+            )
+            return dirs
+        })
+        .then((dirs) => {
+            const all = []
+            const pas = dirs.pop()
+
+            pas.filter((pa) => pa).forEach((pa, idx) => {
+                const tar = tarPath + splitSign + pa.name
+                const src = srcPath + splitSign + pa.name
+                const subs = dirs[idx]
+
+                subs.forEach((file) =>
+                    all.push(
+                        fsp.copyFile(
+                            src + splitSign + file.name,
+                            tar + splitSign + file.name
+                        )
+                    )
+                )
+            })
+
+            return Promise.all(all)
         })
         .catch((err) => {
-            l(`😔: ${err.message}.\n`)///g
+            l(`😔: ${err.message}.\n`) ///g
         })
 }
 
